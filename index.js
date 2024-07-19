4c717566a75823233d9f8b6c8798a634484401b7
Sky Marsh <soras1758@gmail.com>
2024-07-19 21:46:43 +0900

    add validation function. linked js file to the page.

---
+++ index.js
@@ -0,0 +1,9 @@
+const pwAlert=document.createElement('div')
+
+btn.addEventListener('click',()=>{
+    const pw=document.querySelector('#user_password');
+    const confirmPw=document.querySelector('#confirm_password')
+    if(pw.value===confirmPw)
+})
+
+const btn=document.addEventListener('button');
